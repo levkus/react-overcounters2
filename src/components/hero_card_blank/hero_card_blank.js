d5d0cd7f323e011536aec28ac070e9711cbb1633
Lev Ostanin <lev.ostanin@gmail.com>
2016-07-05 17:50:45 +0300

    added section separator

--- src/components/hero_card_blank/hero_card_blank.js
+++ src/components/hero_card_blank/hero_card_blank.js
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
 import styles from './hero_card_blank.scss'
 
-export default () => {
-  return <div className={styles.welcome}>Please, select a hero.</div>
+class HeroCardBlank extends Component {
+  render () {
+    if (!this.props.welcome) {
+      return <div className={styles.welcome}>Select a hero to start.</div>
+    }
+    return <div className={styles.welcome}>{this.props.welcome}</div>
+  }
 }
+
+const mapStateToProps = state => {
+  return {
+    welcome: state.currentLanguage.ui.welcomeMessage
+  }
+}
+
+HeroCardBlank.propTypes = {
+  welcome: PropTypes.string
+}
+
+export default connect(mapStateToProps)(HeroCardBlank)
